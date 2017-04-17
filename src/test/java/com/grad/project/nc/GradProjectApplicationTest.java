2aee1a2ebf8fd34a516984a20659b6b2357a03da
Олександр Геращенко <olexandrgerashchenko@gmail.com>
2017-04-17 18:39:03 +0300

    CI notifications to SLACK

---
+++ src/test/java/com/grad/project/nc/GradProjectApplicationTest.java
@@ -0,0 +1,15 @@
+package com.grad.project.nc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Alex on 4/17/2017.
+ */
+public class GradProjectApplicationTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
