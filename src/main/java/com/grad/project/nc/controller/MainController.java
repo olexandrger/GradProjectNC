7fa10a617a55e0b5b2e7d13452fb3ffe5394ee0a
Ярослав Талеб < >
2017-04-19 06:43:51 +0300

    Created login controller

---
+++ src/main/java/com/grad/project/nc/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.grad.project.nc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
