f1b2933b51ee7ed5affb7feca290571165523ea0
Dmytro Kanivets <dkan327@gmail.com>
2017-04-25 23:56:38 +0300

    Added AbstractDao and DAOs for product order and product instance
    Fixed typo in product order

---
+++ src/main/java/com/grad/project/nc/persistence/AbstractDao.java
@@ -0,0 +1,57 @@
+package com.grad.project.nc.persistence;
+
+import com.grad.project.nc.persistence.exceptions.NonUniqueResultException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+abstract class AbstractDao<T> implements CrudDao<T> {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    AbstractDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    Long getLongValue(KeyHolder keyHolder, String keyName) {
+        return ((Number)keyHolder.getKeys().get(keyName)).longValue();
+    }
+
+    KeyHolder executeInsert(PreparedStatementCreator statementCreator) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(statementCreator, keyHolder);
+        return keyHolder;
+    }
+
+    void executeUpdate(PreparedStatementCreator statementCreator) {
+        jdbcTemplate.update(statementCreator);
+    }
+
+    T findOne(PreparedStatementCreator statementCreator, RowMapper<T> mapper) {
+        List<T> results = jdbcTemplate.query(statementCreator, mapper);
+
+        if (results.size() > 1) {
+            throw new NonUniqueResultException();
+        }
+
+        if (results.size() == 0) {
+            return null;
+        }
+
+        return results.get(0);
+
+    }
+
+    Collection<T> findMultiple(PreparedStatementCreator statementCreator, RowMapper<T> mapper) {
+        return jdbcTemplate.query(statementCreator, mapper);
+    }
+}
--- src/main/java/com/grad/project/nc/persistence/StatusTypeDao.java
+++ src/main/java/com/grad/project/nc/persistence/StatusTypeDao.java
@@ -1,54 +1,44 @@
 package com.grad.project.nc.persistence;
 
 import com.grad.project.nc.model.StatusType;
-import com.grad.project.nc.persistence.exceptions.NonUniqueResultException;
 import com.grad.project.nc.persistence.mappers.StatusTypeRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
-import java.util.List;
 
 @Repository
-public class StatusTypeDao implements CrudDao<StatusType> {
-
-    private JdbcTemplate jdbcTemplate;
+public class StatusTypeDao extends AbstractDao<StatusType> {
 
     @Autowired
-    public StatusTypeDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    StatusTypeDao(JdbcTemplate jdbcTemplate) {
+        super(jdbcTemplate);
     }
 
     @Override
     public StatusType add(StatusType entity) {
-        final String statement = "INSERT INTO status_type (status_type_name) VALUES (?)";
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement preparedStatement = connection.prepareStatement(statement);
+        KeyHolder keyHolder = executeInsert(connection -> {
+            String statement = "INSERT INTO status_type (status_type_name) VALUES (?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS);
 
             preparedStatement.setString(1, entity.getStatusTypeName());
 
             return preparedStatement;
-        }, keyHolder);
+        });
 
-        entity.setStatusTypeId(keyHolder.getKey().longValue());
+        entity.setStatusTypeId(((Number)keyHolder.getKeys().get("status_type_id")).longValue());
 
         return entity;
     }
 
     @Override
     public StatusType update(StatusType entity) {
-        final String statement = "UPDATE status_type SET status_type_name=? WHERE status_type_id=?";
-
-        jdbcTemplate.update(connection -> {
+        executeUpdate(connection -> {
+            String statement = "UPDATE status_type SET status_type_name=? WHERE status_type_id=?";
             PreparedStatement preparedStatement = connection.prepareStatement(statement);
 
             preparedStatement.setString(1, entity.getStatusTypeName());
@@ -62,41 +52,27 @@ public class StatusTypeDao implements CrudDao<StatusType> {
 
     @Override
     public StatusType find(long id) {
-        final String statement = "SELECT status_type_id, status_type_name FROM status_type WHERE status_type_id=?";
-
-        List<StatusType> results = jdbcTemplate.query(connection -> {
+        return findOne(connection -> {
+            String statement = "SELECT status_type_id, status_type_name FROM status_type WHERE status_type_id=?";
             PreparedStatement preparedStatement = connection.prepareStatement(statement);
 
             preparedStatement.setLong(1, id);
 
             return preparedStatement;
         }, new StatusTypeRowMapper());
-
-        if (results.size() > 1) {
-            throw new NonUniqueResultException();
-        }
-
-        if (results.size() == 0) {
-            return null;
-        }
-
-        return results.get(0);
     }
 
     @Override
     public Collection<StatusType> findAll() {
         final String statement = "SELECT status_type_id, status_type_name FROM status_type";
-
-        return jdbcTemplate.query(
-                connection -> connection.prepareStatement(statement),
+        return findMultiple(connection -> connection.prepareStatement(statement),
                 new StatusTypeRowMapper());
     }
 
     @Override
     public void delete(StatusType entity) {
-        final String statement = "DELETE FROM status_type WHERE status_type_id = ?";
-
-        jdbcTemplate.update(connection -> {
+        executeUpdate(connection -> {
+            final String statement = "DELETE FROM status_type WHERE status_type_id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(statement);
 
             preparedStatement.setLong(1, entity.getStatusTypeId());
