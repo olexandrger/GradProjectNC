e25646d978cc8f9182c82370bd1ff3cc4cf8ea6f
Denigam <denigam@gmail.com>
2017-04-30 10:48:29 +0300

    DomainType.java

--- src/main/java/com/grad/project/nc/model/DomainType.java
+++ src/main/java/com/grad/project/nc/model/DomainType.java
@@ -1,25 +1,14 @@
 package com.grad.project.nc.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by Alex on 4/26/2017.
  */
+@Data
+@NoArgsConstructor
 public class DomainType {
     private Long domainTypeId;
     private String domainTypeName;
-
-    public Long getDomainTypeId() {
-        return domainTypeId;
-    }
-
-    public void setDomainTypeId(Long domainTypeId) {
-        this.domainTypeId = domainTypeId;
-    }
-
-    public String getDomainTypeName() {
-        return domainTypeName;
-    }
-
-    public void setDomainTypeName(String domainTypeName) {
-        this.domainTypeName = domainTypeName;
-    }
 }
