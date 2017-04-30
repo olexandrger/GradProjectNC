de135a8ca99410073902f1fb9e5e1effc5515c9f
Denigam <denigam@gmail.com>
2017-04-30 14:33:04 +0300

    upd LocationDao.java create GoogleRegionDao.java

---
+++ src/main/java/com/grad/project/nc/persistence/GoogleRegionDao.java
@@ -0,0 +1,17 @@
+package com.grad.project.nc.persistence;
+
+import com.grad.project.nc.model.GoogleRegion;
+import com.grad.project.nc.model.Location;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by DeniG on 30.04.2017.
+ */
+@Repository
+public class GoogleRegionDao extends AbstractDao<GoogleRegion> {
+
+
+    public GoogleRegion getGoogleRegionByLocation(Location location){
+
+    }
+}
--- src/main/java/com/grad/project/nc/persistence/LocationDao.java
+++ src/main/java/com/grad/project/nc/persistence/LocationDao.java
@@ -1,44 +1,148 @@
 package com.grad.project.nc.persistence;
 
 import com.grad.project.nc.model.Address;
+import com.grad.project.nc.model.GoogleRegion;
 import com.grad.project.nc.model.Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 
 /**
  * Created by DeniG on 30.04.2017.
  */
 @Repository
-public class LocationDao extends AbstractDao{
+public class LocationDao extends AbstractDao<Location> {
 
 
-    public Location getLocationByAddress(Address address){
+    GoogleRegionDao googleRegionDao;
+
+    LocationRowMapper mapper = new LocationRowMapper();
+
+    @Autowired
+    public LocationDao(JdbcTemplate jdbcTemplate, GoogleRegionDao googleRegionDao) {
+        super(jdbcTemplate);
+        this.googleRegionDao = googleRegionDao;
+    }
+
+    public Location getLocationByAddress(Address address) {
+        return findOne(connection -> {
+            final String FIND_QUERY =
+                    "SELECT " +
+                            "l.location_id, " +
+                            "l.google_place_id, " +
+                            "l.google_region_id " +
+                            "FROM location l " +
+                            "WHERE l.location_id = " +
+                                "(SELECT ad.location_id " +
+                                "FROM address ad " +
+                                "WHERE ad.address_id = ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
+            preparedStatement.setLong(1, address.getAddressId());
+            return preparedStatement;
+        }, mapper);
 
     }
 
     @Override
-    public Object add(Object entity) {
-        return null;
+    public Location add(Location entity) {
+        KeyHolder keyHolder = executeInsert(connection -> {
+            final String INSERT_QUERY = "INSERT INTO location (location.google_place_id, location.google_region_id) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY);
+            preparedStatement.setString(1, entity.getGooglePlaceId());
+            preparedStatement.setLong(2, entity.getGoogleRegion().getGoogleRegionId());
+            return preparedStatement;
+        });
+        return find(getLongValue(keyHolder, "location_id"));
     }
 
     @Override
-    public Object update(Object entity) {
-        return null;
+    @Transactional
+    public Location update(Location entity) {
+        executeUpdate(connection -> {
+            final String UPDATE_QUERY =
+                    "UPDATE location " +
+                            "SET " +
+                            "location.google_place_id = ?, " +
+                            "location.google_region_id = ? " +
+                            "WHERE location.location_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+            preparedStatement.setString(1, entity.getGooglePlaceId());
+            preparedStatement.setLong(2, entity.getGoogleRegion().getGoogleRegionId());
+            preparedStatement.setLong(3, entity.getLocationId());
+            return preparedStatement;
+        });
+        return entity;
     }
 
     @Override
-    public Object find(long id) {
-        return null;
+    public Location find(long id) {
+        return findOne(connection -> {
+            final String FIND_QUERY = "SELECT " +
+                    "l.location_id, " +
+                    "l.google_place_id, " +
+                    "FROM location l " +
+                    "WHERE l.location_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+
+        }, mapper);
     }
 
     @Override
-    public Collection findAll() {
-        return null;
+    public Collection<Location> findAll() {
+        return findMultiple(connection -> {
+            final String FIND_QUERY =
+                    "SELECT " +
+                            "l.location_id, " +
+                            "l.google_place_id, " +
+                            "FROM location l ";
+            return connection.prepareStatement(FIND_QUERY);
+        }, mapper);
     }
 
     @Override
-    public void delete(Object entity) {
+    public void delete(Location entity) {
+        executeUpdate(connection -> {
+            final String DELETE_QUERY =
+                    "DELETE " +
+                            "FROM location " +
+                            "WHERE location.location_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
+            preparedStatement.setLong(1, entity.getLocationId());
+            return preparedStatement;
+        });
 
+    }
+
+
+    private class LocationProxy extends Location {
+        @Override
+        public GoogleRegion getGoogleRegion() {
+            if (super.getGoogleRegion() == null) {
+                super.setGoogleRegion(googleRegionDao.getGoogleRegionByLocation(this));
+            }
+            return super.getGoogleRegion();
+        }
+    }
+
+    private final class LocationRowMapper implements RowMapper<Location> {
+
+        @Override
+        public Location mapRow(ResultSet resultSet, int i) throws SQLException {
+            Location location = new LocationProxy();
+            location.setLocationId(resultSet.getLong("location_id"));
+            location.setGooglePlaceId(resultSet.getString("google_place_id"));
+            return location;
+
+        }
     }
 }
