d2eeef1112b03d86a634460ce944ed19aa5d41d1
Denigam <denigam@gmail.com>
2017-04-30 10:46:43 +0300

    DomainTypeDao

--- src/main/java/com/grad/project/nc/persistence/DomainTypeDao.java
+++ src/main/java/com/grad/project/nc/persistence/DomainTypeDao.java
@@ -20,42 +20,37 @@ import java.util.Collection;
  * Created by Alex on 4/26/2017.
  */
 @Repository
-public class DomainTypeDao implements CrudDao<DomainType>  {
+public class DomainTypeDao extends AbstractDao<DomainType> {
 
-    private JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public DomainTypeDao(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
+    public DomainTypeDao(JdbcTemplate jdbcTemplate) {
+        super(jdbcTemplate);
     }
 
     @Override
     public DomainType add(DomainType entity) {
-        final String INSERT_QUERY = "INSERT INTO domain_type(domain_type_name) VALUES (?);";
 
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(
-                new PreparedStatementCreator() {
-                    public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                        PreparedStatement ps =
-                                connection.prepareStatement(INSERT_QUERY, new String[]{"domain_type_id"});
-                        ps.setString(1, entity.getDomainTypeName());
-                        return ps;
-                    }
-                },
-                keyHolder);
-
-        entity.setDomainTypeId(keyHolder.getKey().longValue());
+        KeyHolder keyHolder = executeInsert(connection -> {
+            final String INSERT_QUERY = "INSERT INTO domain_type(domain_type_name) VALUES (?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY);
+            preparedStatement.setString(1, entity.getDomainTypeName());
+            return preparedStatement;
+        });
+        entity.setDomainTypeId(getLongValue(keyHolder, "domain_type_id"));
 
         return entity;
     }
 
     @Override
     public DomainType update(DomainType entity) {
-        final String UPDATE_QUERY = "UPDATE domain_type SET domain_type_name = ? WHERE domain_type_id = ? ";
-
-        jdbcTemplate.update(UPDATE_QUERY, entity.getDomainTypeName(),entity.getDomainTypeId());
+        executeUpdate(connection -> {
+            final String UPDATE_QUERY = "UPDATE domain_type SET domain_type_name = ? WHERE domain_type_id = ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+            preparedStatement.setString(1, entity.getDomainTypeName());
+            preparedStatement.setLong(2, entity.getDomainTypeId());
+            return preparedStatement;
+        });
 
         return entity;
 
@@ -63,35 +58,66 @@ public class DomainTypeDao implements CrudDao<DomainType>  {
 
     @Override
     public DomainType find(long id) {
-        final String SELECT_QUERY = "SELECT domain_type_name FROM domain_type WHERE domain_type_id = ?";
 
+        return findOne(connection -> {
+            final String SELECT_QUERY =
+                    "SELECT " +
+                            "domain_type_name, " +
+                            "domain_type_id " +
+                            "FROM " +
+                            "domain_type " +
+                            "WHERE " +
+                            "domain_type_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        }, new DomainTypeRowMapper());
 
-        DomainType domainType = null;
-        try {
-            domainType = jdbcTemplate.queryForObject(SELECT_QUERY,
-                    new Object[]{id}, new DomainTypeRowMapper());
-        } catch (EmptyResultDataAccessException ex){
-
-        }
-
-        return domainType;
     }
 
     @Override
     public Collection<DomainType> findAll() {
-        final String SELECT_QUERY = "SELECT domain_type_id, domain_type_name  FROM domain_type" ;
-        return jdbcTemplate.query(SELECT_QUERY, new DomainTypeRowMapper());
+        return findMultiple(connection -> {
+            final String SELECT_QUERY =
+                    "SELECT " +
+                            "domain_type_id, " +
+                            "domain_type_name  " +
+                            "FROM domain_type";
+            return connection.prepareStatement(SELECT_QUERY);
+        }, new DomainTypeRowMapper());
 
     }
 
     @Override
     public void delete(DomainType entity) {
-        final String DELETE_QUERY = "DELETE FROM domain_type WHERE domain_type_id = ?";
-        jdbcTemplate.update(DELETE_QUERY,entity.getDomainTypeId());
+        executeUpdate(connection -> {
+            final String DELETE_QUERY = "DELETE FROM domain_type WHERE domain_type_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
+            preparedStatement.setLong(1, entity.getDomainTypeId());
+            return preparedStatement;
+        });
 
     }
 
-    private static final class DomainTypeRowMapper implements RowMapper<DomainType>{
+    public DomainType getDomainTypeByDomain(Domain domain) {
+        return findOne(connection -> {
+            //language=GenericSQL
+            final String SELECT_QUERY = "SELECT " +
+                    "domain_type_name, " +
+                    "domain_type_id  " +
+                    "FROM domain_type " +
+                    "WHERE domain_type_id = (" +
+                    "SELECT d.domain_type_id " +
+                    "FROM domain d " +
+                    "WHERE d.domain_id = ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
+            preparedStatement.setLong(1, domain.getDomainId());
+            return preparedStatement;
+        }, new DomainTypeRowMapper());
+
+    }
+
+    private final class DomainTypeRowMapper implements RowMapper<DomainType> {
 
         @Override
         public DomainType mapRow(ResultSet rs, int rowNum) throws SQLException {
