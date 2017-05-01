ad73b9346b28376ad3449953b478c2053aeea182
Denigam <denigam@gmail.com>
2017-05-01 13:59:21 +0300

    upd Discount

--- src/main/java/com/grad/project/nc/model/Discount.java
+++ src/main/java/com/grad/project/nc/model/Discount.java
@@ -1,55 +1,23 @@
 package com.grad.project.nc.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 
 /**
  * Created by Alex on 4/24/2017.
  */
+
+@Data
+@NoArgsConstructor
 public class Discount {
     private Long discountId;
     private String discountTitle;
     private Double discount;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
-
-    public Long getDiscountId() {
-        return discountId;
-    }
-
-    public void setDiscountId(Long discountId) {
-        this.discountId = discountId;
-    }
-
-    public String getDiscountTitle() {
-        return discountTitle;
-    }
-
-    public void setDiscountTitle(String discountTitle) {
-        this.discountTitle = discountTitle;
-    }
-
-    public double getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(double discount) {
-        this.discount = discount;
-    }
-
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDateTime getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
+    private Collection<ProductRegionPrice> productRegionPrices;
 }
