ad51a7899227fb705fc984a3c92426b7662c872d
ihorloboda <igorloboda9@gmail.com>
2017-05-11 17:04:33 +0300

    some minor changes

--- src/main/java/com/grad/project/nc/service/locations/LocationService.java
+++ src/main/java/com/grad/project/nc/service/locations/LocationService.java
@@ -2,11 +2,15 @@ package com.grad.project.nc.service.locations;
 
 public interface LocationService {
     //after this method you can obtain regionName, locationId, lat, lng
-    boolean doRequestForJSON(String address);
+    LocationService doRequestForJSONByAddress(String address);
+    LocationService doRequestForJSONByGooglePlaceId(String id);
     String getFullAddress(String language);
     String getRegionName();
-    String getLocationId();
+    String getGooglePlaceId();
     double getRegionLng();
     double getRegionLat();
+    String getStreet();
+    String getBuildingNumber();
+    String getCity();
 }
 
