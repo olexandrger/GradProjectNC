93e6395f3c758943da1cb3b09d3e090c95459652
ihorloboda <igorloboda9@gmail.com>
2017-05-18 03:43:28 +0300

    Added method deleteDomainUsers() to DomainDao

--- src/main/java/com/grad/project/nc/persistence/DomainDao.java
+++ src/main/java/com/grad/project/nc/persistence/DomainDao.java
@@ -1,6 +1,7 @@
 package com.grad.project.nc.persistence;
 
 import com.grad.project.nc.model.Domain;
+import com.grad.project.nc.model.User;
 
 import java.util.List;
 
@@ -9,6 +10,8 @@ public interface DomainDao extends CrudDao<Domain> {
     List<Domain> findByUserId(Long userId);
 
     void deleteDomainUser(Long domainId, Long userId);
+
+    void deleteDomainUsers(Long domainId);
 
     void addDomainUser(Long domainId, Long userId);
 }
