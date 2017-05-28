72dc0213d65b0f39f04d6eed77fbec4aa40dbc43
Олександр Геращенко <olexandrgerashchenko@gmail.com>
2017-05-28 21:49:35 +0300

    Fixes in user editing.

--- src/main/java/com/grad/project/nc/controller/api/dto/instance/FrontendInstanceAddress.java
+++ src/main/java/com/grad/project/nc/controller/api/dto/instance/FrontendInstanceAddress.java
@@ -1,10 +1,8 @@
 package com.grad.project.nc.controller.api.dto.instance;
 
-import com.grad.project.nc.model.Address;
-import com.grad.project.nc.service.locations.LocationService;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 /**
  * Created by Alex on 5/27/2017.
@@ -12,10 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Data
 @Builder
 public class FrontendInstanceAddress {
-
-    @Autowired
-    LocationService locationService;
-
 
     private String apartment;
     private String city;
